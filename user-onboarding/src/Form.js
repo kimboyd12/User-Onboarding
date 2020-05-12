9a9ccab91de8a9b6dbe6512ea5ab3de17d7d7541
Kimberly Boyd <kimberlyboyd@Kimberlys-MacBook-Pro.local>
2020-05-12 13:56:10 -0400

    form, validation, and errors are functioning

--- user-onboarding/src/Form.js
+++ user-onboarding/src/Form.js
@@ -2,7 +2,14 @@ import React, { useState } from "react";
 import * as yup from "yup";
 import axios from "axios";
 
-// need to import form to App
+
+const formSchema = yup.object().shape({
+    name: yup.string().required("Please enter your name"),
+    email: yup.string().email("Must be a valid email address").required("Please enter your email address"),
+    password: yup.string().required("Please enter your password").min(8, "Password must be at least 8 characters"),
+    terms: yup.boolean().oneOf([true], "Please agree to the Terms and Conditions")
+});
+
 
 export default function Form() {
 
@@ -13,12 +20,47 @@ export default function Form() {
         terms: false
     });
 
-    const changeHandler = e => {
+    const [errorState, setErrorState] = useState({
+        name: "",
+        email: "",
+        password: "",
+        terms: ""
+    });
 
+    const validate = e => {
+        yup 
+            .reach(formSchema, e.target.name)
+            .validate(e.target.value)
+            .then(valid => {
+                setErrorState({
+                    ...errorState, [e.target.name]: ""
+                });
+            })
+            .catch(err => {
+                console.log(err.errors);
+                setErrorState({
+                    ...errorState,
+                    [e.target.name]: err.errors[0]
+                });
+            });
+    };
+
+    const changeHandler = e => {
+        e.persist();
+        validate(e);
+        let value = e.target.type === "checkbox" ? e.target.checked : e.target.value;
+
+        setFormState({ ...formState, [e.target.name]: value })
     }
 
     const formSubmit = e => {
+        e.preventDefault();
+        console.log("form submitted!")
 
+        axios
+            .post("https://reqres.in/api/users", formState)
+            .then(response => console.log(response))
+            .catch(err => console.log(err));
     }
 
     return (
@@ -42,6 +84,7 @@ export default function Form() {
                     value={formState.email}
                     onChange={changeHandler}
                 />
+                {errorState.email.length > 0 ?( <p className="error">{errorState.email}</p>) : null}
             </label>
             <label htmlFor="password">
                 Password:
@@ -52,6 +95,7 @@ export default function Form() {
                     value={formState.password}
                     onChange={changeHandler}
                 />
+                {errorState.password.length > 0 ?( <p className="error">{errorState.password}</p>) : null}
             </label>
             <label htmlFor="terms">
                 I have read the Terms and Conditions
